87a7238b73e8903cc2060e736310442fb959edc4
Skanander <46889106+Skanander@users.noreply.github.com>
2024-09-24 11:51:21 +0200

    some visual tests

--- js/app.js
+++ js/app.js
@@ -8,5 +8,14 @@ window.addEventListener("DOMContentLoaded", async() => {
     setInterval( () => {
         heading = compass.getHeading();
         document.querySelector("#bearing").innerHTML = heading.toString();
+
+        if (heading > 0 && heading < 100) {
+            document.body.style.backgroundColor = "yellow";
+        } else if (heading > 100 && heading < 200) {
+            document.body.style.backgroundColor = "lightblue";
+        } else if (heading > 200) {
+            document.body.style.backgroundColor = "lightgreen";
+        }
+
     }, 2000);
 });
