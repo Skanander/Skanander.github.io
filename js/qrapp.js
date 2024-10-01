fa5cc6fc01dc5d32f2888eeb381bb2f61f629f6e
Skanander <46889106+Skanander@users.noreply.github.com>
2024-10-01 13:02:46 +0200

    More styling, QR-reader can be closed

--- js/qrapp.js
+++ js/qrapp.js
@@ -2,6 +2,11 @@ const slideLayout = ["intro", "stol", "kata"];
 
 const html5QrCode = new Html5Qrcode("reader");
 
+let cameraOn = false;
+
+const qrIcon = `<img src="assets/qr.png" width="24" height="24" />`;
+const xIcon  = `<img src="assets/close.png" width="24" height="24" />`;
+
 const qrCodeSuccessCallback = (decodedText, decodedResult) => {
     console.log(decodedText);
     for (let i=0; i<slideLayout.length; i++) {
@@ -11,13 +16,26 @@ const qrCodeSuccessCallback = (decodedText, decodedResult) => {
     }
     html5QrCode.stop().then((ignore) => {
         // Stopped
-      }).catch((err) => {
+        cameraOn = false;
+    }).catch((err) => {
         // Stop failed, handle it.
-      });
+    });
 };
 
 const config = { fps: 10, qrbox: { width: 250, height: 250 } };
 
 document.querySelector("#reader-btn").onclick = function() {
-    html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+    if (!cameraOn) {
+        cameraOn = true;
+        this.innerHTML = xIcon;
+        html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+    } else {
+        html5QrCode.stop().then((ignore) => {
+            // Stopped
+            cameraOn = false;
+            document.querySelector("#reader-btn").innerHTML = qrIcon;
+        }).catch((err) => {
+            // Stop failed, handle it.
+        });
+    }
 }
