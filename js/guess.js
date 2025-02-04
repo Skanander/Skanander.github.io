cd35e4792605d12a7a2f9e41007ceffe36f5c75e
Skanander <46889106+Skanander@users.noreply.github.com>
2025-02-04 15:29:36 +0100

    update

--- js/guess.js
+++ js/guess.js
@@ -95,19 +95,20 @@ function GuessingGame() {
         }
         
         questionImage.src = question.questionImage;
+        questionImage.classList.add("in");
 
         if (firstRound === true) {
             startScreen.style.display = "none";
-            questionScreen.classList.add("scale-in-center");
-            answerScreen.classList.add("scale-in-center");
             questionScreen.style.display = "flex";
             answerScreen.style.display = "grid";
             firstRound = false;
+            self.zoom([questionScreen, nextScreen, answerScreen], false);
         } else {
             // Reset colors
             for (let i=0; i<answerCards.length; i++) {
                 answerCards[i].style.cssText = "background-color: white;";
             }
+            self.zoom([nextScreen, answerScreen], true);
         }
 
         self.setupAnswers(question);
@@ -118,18 +119,11 @@ function GuessingGame() {
             window.location.reload();
         }
         btnNext.disabled = true;
+        questionImage.classList.remove("in");
         setTimeout(function() {
-            nextScreen.classList.remove("scale-in-center");
-            nextScreen.classList.add("scale-out-center");
-            questionImage.classList.remove("in");
-            setTimeout(function() {
-                self.start();
-                answerScreen.classList.remove("scale-out-center");
-                answerScreen.classList.add("scale-in-center");
-                questionImage.classList.add("in");
-                questionIcon.innerHTML = "?";
-            }, 500);
-        }, 800);
+            self.start();
+            questionIcon.innerHTML = "?";
+        }, 500);
     }
 
     this.setupAnswers = function(question) {
@@ -139,8 +133,6 @@ function GuessingGame() {
         answers.length = 3;
         answers.push(question);
         self.shuffle(answers);
-        console.log(answers);
-        console.log(answerCards);
         for (let i=0; i<4; i++) {
             answerCards[i].querySelector(".answer-label-name").innerHTML = answers[i].name;
             answerCards[i].style.backgroundImage = "url('" + answers[i].answerImage + "')";
@@ -156,18 +148,14 @@ function GuessingGame() {
             element.style.backgroundColor = "var(--green)";
             // Add answer to list of asked questions
             askedQuestions.push(answer);
+            questionTrivia.innerHTML = answer.trivia;
             setTimeout(function() {
-                answerScreen.classList.remove("scale-in-center");
-                answerScreen.classList.add("scale-out-center");
-                questionTrivia.innerHTML = answer.trivia;
-                setTimeout(function() {
-                    if (nextScreen.style.display === "none") {
-                        nextScreen.style.display = "flex";
-                    }
-                    questionIcon.innerHTML = "!";
-                    nextScreen.classList.add("scale-in-center");
-                }, 800);
-            }, 800);
+                if (nextScreen.style.display === "none") {
+                    nextScreen.style.display = "flex";
+                }
+                questionIcon.innerHTML = "!";
+                self.zoom([nextScreen, answerScreen], true);
+            }, 500);
         } else {
             element.style.backgroundColor = "var(--red)";
         }
@@ -200,6 +188,37 @@ function GuessingGame() {
         });
     }
 
+    this.zoom = function(els, delay) {
+        if (delay) {
+            for (i=0; i<els.length; i++) {
+                if (els[i].classList.contains("animate__zoomIn")) {
+                    els[i].classList.remove("animate__zoomIn");
+                    setTimeout(function(el) {
+                        el.classList.add("animate__zoomOut");
+                        el.style.visibility = "hidden";
+                    }, 200, els[i]);
+                } else {
+                    els[i].classList.remove("animate__zoomOut");
+                    setTimeout(function(el) {
+                        el.classList.add("animate__zoomIn");
+                        el.style.visibility = "visible";
+                    }, 200, els[i]);
+                }
+            }
+        } else {
+            for (i=0; i<els.length; i++) {
+                if (els[i].classList.contains("animate__zoomIn")) {
+                    els[i].classList.remove("animate__zoomIn");
+                    els[i].classList.add("animate__zoomOut");
+                    els[i].style.visibility = "hidden";
+                } else {
+                    els[i].classList.remove("animate__zoomOut");
+                    els[i].classList.add("animate__zoomIn");
+                    els[i].style.visibility = "visible";
+                }
+            }
+        }
+    }
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
