b980678e1f26c80b3a15f4c5b1381805725b030b
Patrik Erlandsson <patrik@localhost.localdomain>
2025-02-20 20:25:40 +0100

    update poop game

--- js/guessingGame.js
+++ js/guessingGame.js
@@ -1,8 +1,191 @@
 function GuessingGame() {
     const self = this;
+    const triviaScreen = document.getElementById("triviaContainer");
+    const triviaText = document.getElementById("triviaText");
+    const questionImage = document.getElementById("questionImage");
+    const answerCards = document.querySelectorAll(".answerButton");
+
+    let questions = [
+        bjorn = {
+            answerImage: "assets/guess/bear.jpg",
+            questionImage: "assets/guess/poop/bjorn.png",
+            name: "Björn",
+            trivia: "Björnen äter mest växter eller bär, men ibland äter den kött och spillningen blir då annorlunda.",
+        },
+        hare = {
+            answerImage: "assets/guess/hare.webp",
+            questionImage: "assets/guess/poop/hare.png",
+            name: "Hare",
+            trivia: "Trivia.",
+        },
+        fladdermus = {
+            answerImage: "assets/guess/fladdermus.jpg",
+            questionImage: "assets/guess/poop/fladdermus.png",
+            name: "Fladdermus",
+            trivia: "Trivia.",
+        },
+        rav = {
+            answerImage: "assets/guess/rav.jpg",
+            questionImage: "assets/guess/poop/rav.png",
+            name: "Räv",
+            trivia: "Trivia.",
+        },
+        radjur = {
+            answerImage: "assets/guess/radjur.jpg",
+            questionImage: "assets/guess/poop/radjur.png",
+            name: "Rådjur",
+            trivia: "Eftersom rådjur idisslar ser deras bajs nästan alltid likadant ut.",
+        },
+        alg = {
+            answerImage: "assets/guess/alg.jpg",
+            questionImage: "assets/guess/poop/alg.png",
+            name: "Älg",
+            trivia: "Eftersom älgar idisslar ser deras bajs nästan alltid likadant ut.",
+        },
+        skogsmus = {
+            answerImage: "assets/guess/skogsmus.jpg",
+            questionImage: "assets/guess/poop/skogsmus.png",
+            name: "Skogsmus",
+            trivia: "Trivia.",
+        },
+        tjader = {
+            answerImage: "assets/guess/tjader.jpg",
+            questionImage: "assets/guess/poop/tjader.png",
+            name: "Tjäder",
+            trivia: "Trivia.",
+        },
+        varg = {
+            answerImage: "assets/guess/varg.jpg",
+            questionImage: "assets/guess/poop/varg.png",
+            name: "Varg",
+            trivia: "Vargspillning är ganska lik hundars, men innehåller ofta hår.",
+        },
+        vildsvin = {
+            answerImage: "assets/guess/vildsvin.jpg",
+            questionImage: "assets/guess/poop/vildsvin.png",
+            name: "Vildsvin",
+            trivia: "Trivia.",
+        },
+        orre = {
+            answerImage: "assets/guess/orre.jpg",
+            questionImage: "assets/guess/poop/orre.png",
+            name: "Orre",
+            trivia: "Trivia.",
+        },
+        utter = {
+            answerImage: "assets/guess/utter.jpg",
+            questionImage: "assets/guess/poop/utter.png",
+            name: "Utter",
+            trivia: "Trivia.",
+        },
+        kanin = {
+            answerImage: "assets/guess/kanin.jpg",
+            questionImage: "assets/guess/poop/kanin.png",
+            name: "Kanin",
+            trivia: "Trivia.",
+        },
+    ];
+    
+    let firstRound = true;
+    let askedQuestions = [];
 
     this.init = function() {
         console.log("init");
+        self.start();
+    }
+
+    this.start = function() {
+        let question;
+        let foundQuestion = false;
+
+        while (!foundQuestion) {
+            question = self.randomQuestion(questions);
+            console.log("Question", question);
+            if (!self.checkAvailability(askedQuestions, question)) {
+                foundQuestion = true;
+            }
+        }
+        
+        questionImage.src = question.questionImage;
+
+        if (firstRound === true) {
+            firstRound = false;
+        } else {
+            // Reset colors
+            for (let i=0; i<answerCards.length; i++) {
+                answerCards[i].style.cssText = "background-color: var(--green-400);";
+            }
+        }
+        self.setupAnswers(question);
+    }
+
+    this.next = function() {
+        triviaScreen.onclick = void(0);
+        animateCSS(triviaScreen, "slideOutUp").then((message) => {
+            triviaScreen.style.display = "none";
+            if (askedQuestions.length === questions.length) {
+                window.location.reload();
+            } else {
+                self.start();
+            }
+        });
+    }
+
+    this.setupAnswers = function(question) {
+        let answers = structuredClone(questions);
+        answers = answers.filter(function(item) { return item.name != question.name; });
+        self.shuffle(answers);
+        answers.length = 3;
+        answers.push(question);
+        self.shuffle(answers);
+        for (let i=0; i<4; i++) {
+            answerCards[i].querySelector(".answerName").innerHTML = answers[i].name;
+            answerCards[i].querySelector(".answerImage").src = answers[i].answerImage;
+            answerCards[i].onclick = function() {
+                self.checkAnswer(question, answers[i], answerCards[i]);
+            }
+        }
+    }
+
+    this.checkAnswer = function(question, answer, element) {
+        if (answer.name === question.name) {
+            element.style.backgroundColor = "limegreen";
+            // Add answer to list of asked questions
+            askedQuestions.push(answer);
+            triviaText.innerHTML = answer.trivia;
+            triviaScreen.style.display = "flex";
+            animateCSS(triviaScreen, "slideInDown");
+            triviaScreen.onclick = self.next;
+        } else {
+            element.style.backgroundColor = "var(--red-500)";
+        }
+    }
+
+    this.randomQuestion = function(array) {
+        let i = Math.floor(Math.random() * array.length);
+        let r = array[i];
+        return r;
+    }
+
+    this.shuffle = function(array) {
+        let currentIndex = array.length;
+        
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+        
+            // Pick a remaining element...
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+        
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+    }
+
+    this.checkAvailability = function(arr, val) {
+        return arr.some(function (arrVal) {
+            return val === arrVal;
+        });
     }
 }
 
